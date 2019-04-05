2703c22fbdd22df0ac628aecb28357fa520069e7
下川　純 <shimokawajun@shimokawajun-no-MacBook-Air.local>
2019-04-06 06:15:10 +0900

    my first commit

---
+++ hangman.py
@@ -0,0 +1,20 @@
+
+
+def hangan(word):
+    wrong = 0
+    stages = ["",
+              "______     ",
+              "|          ",
+              "|     |    ",
+              "|     o    ",
+              "|    /|\   ",
+              "|    / \   ",
+              "|          "
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("ハングマンへようこそ！")
+
+    while wrong 
+    
